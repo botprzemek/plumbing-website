9022d2ea324c22c972f4f7b08d2b211a905b9915
botprzemek <info@botprzemek.pl>
2023-07-02 12:26:54 +0200

    Footer

--- src/components/WidgetSection.vue
+++ src/components/WidgetSection.vue
@@ -6,7 +6,7 @@ import {mobileCheck} from '@/methods';
 
 <template>
   <aside class="sticky bottom-0 z-50 grid place-items-end">
-    <button class="px-6 py-1.5 gap-1 bg-black h-fit w-fit rounded-full hover:scale-105 transition-transform duration-300 ease-in-out -translate-x-3 -translate-y-3 shadow-sm">
+    <button title="Phone widget" class="px-6 py-1.5 gap-1 bg-black h-fit w-fit rounded-full hover:scale-105 transition-transform duration-300 ease-in-out -translate-x-3 -translate-y-3 shadow-sm">
       <a :href="`tel:${contacts.phone.link}`" class="text-base sm:text-base grid gap-2 grid-flow-col place-items-center" tabindex="-1">
         <span v-if="!mobileCheck()" class="text-white">Zadzwoń teraz!</span>
         <PhoneIcon/>
